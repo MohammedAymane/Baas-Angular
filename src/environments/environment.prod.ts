fac9aa51ee2811a7c558b0cfecdce12097f2a1c0
Ouggadi Mohammed Aymane <58845118+MohammedAymane@users.noreply.github.com>
2021-05-24 22:09:43 +0100

    Revert "Update environment.prod.ts"
    This reverts commit 8a59636b71a1ee7482cdd5f0a1fda0cb8b3037b7.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyBaCJOzlduCDrEI9Lp0EGBFmaM1MwJuguI",
-    authDomain: "paasangular.firebaseapp.com",
-    projectId: "paasangular",
-    storageBucket: "paasangular.appspot.com",
-    messagingSenderId: "29216946188",
-    appId: "1:29216946188:web:277c17abd8c41ab9161629"
-  }
+  production: true
 };
