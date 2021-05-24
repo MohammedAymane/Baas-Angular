e1572a013b6722ecb606eae9b79ae6df4b6b612d
Ouggadi Mohammed Aymane <58845118+MohammedAymane@users.noreply.github.com>
2021-05-24 22:16:47 +0100

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBaCJOzlduCDrEI9Lp0EGBFmaM1MwJuguI",
+    authDomain: "paasangular.firebaseapp.com",
+    projectId: "paasangular",
+    storageBucket: "paasangular.appspot.com",
+    messagingSenderId: "29216946188",
+    appId: "1:29216946188:web:277c17abd8c41ab9161629"
+  }
 };
