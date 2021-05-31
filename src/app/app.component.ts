70b79ff67628187ed7f49257f81d169c634a2544
Ouggadi Mohammed Aymane <58845118+MohammedAymane@users.noreply.github.com>
2021-05-31 22:28:28 +0100

    Google Authentification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { AuthService } from './service/auth.service';
+import { SocialAuthService, GoogleLoginProvider, SocialUser } from 'angularx-social-login';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,12 +15,27 @@ export class AppComponent {
   password:string;
   data:any = {};
 
-  constructor(public authService: AuthService, private _httpClient: HttpClient){
+  loginForm: FormGroup | undefined;
+  socialUser: SocialUser = new SocialUser;
+  isLoggedin: boolean = false;  
+
+  constructor(public authService: AuthService, private _httpClient: HttpClient, private formBuilder: FormBuilder, private socialAuthService: SocialAuthService){
     this.email='';
     this.password='';
   }
   ngOnInit(){
     this.getData();
+    
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    this.socialAuthService.authState.subscribe((user) => {
+      this.socialUser = user;
+      this.isLoggedin = (user != null);
+      console.log(this.socialUser);
+    });
   };
 
   signUp(){
@@ -44,5 +61,15 @@ export class AppComponent {
       this.data = dataFirestore;
     })
   }
+
+  loginWithGoogle(): void {
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
+    alert('logged in');
+  }
+
+  logOut2(): void {
+    this.socialAuthService.signOut();
+  }
+  
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireMessagingModule } from '@angular/fire/messaging';
 import { AngularFireStorageModule } from '@angular/fire/storage';
+
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import { GoogleLoginProvider } from 'angularx-social-login';
 
 import { HttpClientModule } from '@angular/common/http';
 
@@ -24,9 +27,27 @@ import { environment } from 'src/environments/environment';
     AngularFireAuthModule,
     AngularFireMessagingModule,
     AngularFireStorageModule,
-    HttpClientModule
+    HttpClientModule,
+    SocialLoginModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '229836909682-aujrdh1ccu14gkc0ts7hvkagr9629urv.apps.googleusercontent.com'
+            )
+          }
+        ]
+      } as SocialAuthServiceConfig,
+    }  
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
